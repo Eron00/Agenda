15a4b3faeec420aad9ecbf91ea1402be13398907
Eron00 <eron.silva@fatec.sp.gov.br>
2018-03-16 19:33:42 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -0,0 +1,56 @@
+package br.com.alura.agenda;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.modelo.Aluno;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+        helper = new FormularioHelper(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+
+                Aluno aluno = helper.pegaAluno();
+                AlunoDAO alunoDAO = new AlunoDAO(this);
+                alunoDAO.insereAluno(aluno);
+
+                alunoDAO.close();
+
+                Toast.makeText(this, "Aluno " + aluno.getNome() +" salvo!!!", Toast.LENGTH_LONG).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -0,0 +1,99 @@
+package br.com.alura.agenda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.modelo.Aluno;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    private ListView listaAlunos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+        listaAlunos = (ListView) findViewById(R.id.ListaAlunos);
+
+        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> lista, View item, int position, long id) {
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(position);
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+/*
+        listaAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListaAlunosActivity.this, "CliCk longo!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+*/
+
+        registerForContextMenu(listaAlunos);
+    }
+
+    public void listaAlunos_btnNovo(View view){
+        Intent IrParaFormulario = new Intent(ListaAlunosActivity.this,FormularioActivity.class);
+        startActivity(IrParaFormulario);
+    }
+
+    private void carregaLista() {
+        AlunoDAO alunoDAO = new AlunoDAO(this);
+        List<Aluno> alunos = alunoDAO.buscarAlunos();
+        alunoDAO.close();
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregaLista();
+    }
+
+    @Override
+    public void onCreateContextMenu(final ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem deletar = menu.add("Deletar");
+        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+                AlunoDAO alunoDao = new  AlunoDAO(ListaAlunosActivity.this);
+                alunoDao.deleta(aluno);
+                alunoDao.close();
+
+                carregaLista();
+
+                return false;
+            }
+        });
+
+
+
+
+
+    }
+
+
+}
