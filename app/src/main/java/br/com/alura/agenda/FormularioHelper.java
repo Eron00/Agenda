1affc929bed0b434b39503bb2cb63a450e8d5724
Eron00 <eron.silva@fatec.sp.gov.br>
2018-10-06 03:24:19 -0300

    criação do método para carregar a imagem de cada aluno

--- app/src/main/java/br/com/alura/agenda/FormularioHelper.java
+++ app/src/main/java/br/com/alura/agenda/FormularioHelper.java
@@ -1,8 +1,9 @@
 package br.com.alura.agenda;
 
-import android.view.View;
-import android.widget.Button;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 
 import br.com.alura.agenda.modelo.Aluno;
@@ -17,14 +18,16 @@ public class FormularioHelper {
     private final EditText campoTelefone;
     private final EditText campoSite;
     private final RatingBar campoNota;
+    private final ImageView campoFoto;
     private Aluno aluno;
 
-    public FormularioHelper(FormularioActivity activity){
-        campoNome       = (EditText) activity.findViewById(R.id.formulario_Nome);
-        campoEndereco   = (EditText) activity.findViewById(R.id.formulario_Endereco);
-        campoTelefone   = (EditText) activity.findViewById(R.id.formulario_Telefone);
-        campoSite       = (EditText) activity.findViewById(R.id.formulario_Site);
-        campoNota       = (RatingBar)activity.findViewById(R.id.formulario_RatingBar);
+    FormularioHelper(FormularioActivity activity){
+        campoNome       = activity.findViewById(R.id.formulario_Nome);
+        campoEndereco   = activity.findViewById(R.id.formulario_Endereco);
+        campoTelefone   = activity.findViewById(R.id.formulario_Telefone);
+        campoSite       = activity.findViewById(R.id.formulario_Site);
+        campoNota       = activity.findViewById(R.id.formulario_RatingBar);
+        campoFoto       = activity.findViewById(R.id.formulario_foto);
         aluno           = new Aluno();
     }
 
@@ -35,7 +38,8 @@ public class FormularioHelper {
         aluno.setEndereco(campoEndereco.getText().toString());
         aluno.setTelefone(campoTelefone.getText().toString());
         aluno.setSite(campoSite.getText().toString());
-        aluno.setNota(Double.valueOf(campoNota.getProgress()));
+        aluno.setNota((double) campoNota.getProgress());
+        aluno.setCaminhoFoto((String) campoFoto.getTag());
 
         return aluno;
     }
@@ -47,6 +51,18 @@ public class FormularioHelper {
         campoTelefone.setText(aluno.getTelefone());
         campoSite.setText(aluno.getSite());
         campoNota.setProgress(aluno.getNota().intValue());
+        carregaImagem(aluno.getCaminhoFoto());
+
         this.aluno = aluno;
+    }
+
+    public void carregaImagem(String caminhoFoto) {
+        if(caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+            campoFoto.setTag(caminhoFoto);
+        }
     }
 }
