f534e340fd635e55005235f3efbb908d9cdae431
Eron00 <eron.silva@fatec.sp.gov.br>
2018-10-06 03:20:36 -0300

    retorno da intent da câmera

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -1,29 +1,42 @@
 package br.com.alura.agenda;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.Toolbar;
+
+import java.io.File;
 
 import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.modelo.Aluno;
 
+
+
 public class FormularioActivity extends AppCompatActivity {
 
+    public static final int ATIVAR_CAMERA = 2;
+    public static final int CODIGO_SMS = 3;
     private FormularioHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        if(checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, CODIGO_SMS);
+        }
 
         helper = new FormularioHelper(this);
 
@@ -35,8 +48,27 @@ public class FormularioActivity extends AppCompatActivity {
 
         }
 
+        Button botaoFoto = findViewById(R.id.formulario_botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                caminhoFoto = getExternalFilesDir(null) +"/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivityForResult(intentCamera, ATIVAR_CAMERA);
 
+            }
+        });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK) {
+            if (requestCode == ATIVAR_CAMERA) {
+               helper.carregaImagem(caminhoFoto);
+            }
+        }
     }
 
     @Override
