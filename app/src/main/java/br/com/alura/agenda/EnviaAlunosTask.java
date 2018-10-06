008f656ae065a4b36423284d8e4b8e0755a37340
Eron00 <eron.silva@fatec.sp.gov.br>
2018-10-06 04:22:39 -0300

    Criação da classe que realiza de forma assincrona a requisição com o servidor (Asynctask)

---
+++ app/src/main/java/br/com/alura/agenda/EnviaAlunosTask.java
@@ -0,0 +1,47 @@
+package br.com.alura.agenda;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.util.List;
+
+import br.com.alura.agenda.converter.AlunoConverter;
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.modelo.Aluno;
+
+public class EnviaAlunosTask extends AsyncTask<Object,Object,String> {
+    private Context context;
+    private ProgressDialog progressDialog;
+
+    public EnviaAlunosTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(context,"Aguarde!!!", "Enviando para o servidor",true,true);
+
+    }
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        WebClient client = new WebClient();
+        AlunoConverter conversor = new AlunoConverter();
+        AlunoDAO dao = new AlunoDAO(context);
+        List<Aluno> alunos = dao.buscarAlunos();
+        dao.close();
+        String json = conversor.converterParaJson(alunos);
+        String resposta = client.post(json);
+        return resposta;
+    }
+
+    @Override
+    protected void onPostExecute(String resposta) {
+        progressDialog.dismiss();
+        Toast.makeText(context, resposta, Toast.LENGTH_LONG).show();
+    }
+}
