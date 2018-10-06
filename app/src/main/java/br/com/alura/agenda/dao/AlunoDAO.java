840707ee701aace2210ebf4ed2bef8632ae4f50a
Eron00 <eron.silva@fatec.sp.gov.br>
2018-10-06 03:19:47 -0300

    Implementando o método onUpgrade e o método que verifica se o aluno está registrado no banco

--- app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteOutOfMemoryException;
 import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
@@ -21,22 +20,37 @@ public class AlunoDAO extends SQLiteOpenHelper {
 
 
     public AlunoDAO(Context context) {
-        super(context, "Agenda", null, 1);
+        super(context, "Agenda", null, 2);
 
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, endereco TEXT, telefone TEXT, site TEXT, nota REAL);";
+        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, endereco TEXT, telefone TEXT, site TEXT, nota REAL, caminhoFoto TEXT );";
         db.execSQL(sql);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String sql = "DROP TABLE IF EXISTS Alunos ";
-        db.execSQL(sql);
-        onCreate(db);
+        String sql;
+        switch (oldVersion)
+        {
+            case 1:
+             sql = "ALTER TABLE Alunos ADD COLUMN caminhoFoto TEXT";
+             db.execSQL(sql);
+                //caso exista uma nova versão para o banco, incluir a próxima instrução sql sem o break;
+                // pois assim ele atualizará o banco a partir da versão necessária Exemplo:
+                /*
+                * case 2:
+                * sql= ALTER TABLE Alunos ADD COLUMN estadoCivil TEXT
+                * db.execSQL(sql);
+                * case 3:
+                * sql=........
+                *
+                * */
+
+        }
     }
 
     public void insereAluno(Aluno aluno) {
@@ -56,6 +70,7 @@ public class AlunoDAO extends SQLiteOpenHelper {
         dados.put("telefone",aluno.getTelefone());
         dados.put("site",aluno.getSite());
         dados.put("nota",aluno.getNota());
+        dados.put("caminhoFoto",aluno.getCaminhoFoto());
         return dados;
     }
 
@@ -75,6 +90,7 @@ public class AlunoDAO extends SQLiteOpenHelper {
             aluno.setTelefone(c.getString(c.getColumnIndex("telefone")));
             aluno.setSite(c.getString(c.getColumnIndex("site")));
             aluno.setNota(c.getDouble(c.getColumnIndex("nota")));
+            aluno.setCaminhoFoto(c.getString(c.getColumnIndex("caminhoFoto")));
 
             alunos.add(aluno);
         }
@@ -98,4 +114,12 @@ public class AlunoDAO extends SQLiteOpenHelper {
         db.update("Alunos", dados,"id = ?", params);
 
     }
+
+    public boolean ehAluno(String telefone){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM Alunos WHERE telefone =?", new String[]{telefone} );
+        int resultado = c.getCount();
+        return resultado > 0;
+    }
+
 }
