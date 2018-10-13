01eb458d4597f65ded06dbff81aaa1b63e31702d
Eron00 <eron.silva@fatec.sp.gov.br>
2018-10-13 04:28:58 -0300

    implementando o novo menu para baixar as provas (redirecionar para activity)

--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -82,6 +82,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
             case R.id.menu_enviar_notas:
                 new EnviaAlunosTask(this).execute();
                 break;
+            case R.id.menu_baixar_notas:
+                Intent vaiParaProvas = new Intent(this, ProvasActivity.class);
+                startActivity(vaiParaProvas);
+                break;
 
         }
         return super.onOptionsItemSelected(item);
